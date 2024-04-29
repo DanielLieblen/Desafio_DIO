d0945ab01fa1fd215d0df4742d5a965bd583baa2
DanielLieblen <plocks.me@gmail.com>
2024-04-29 17:22:00 -0300

    modificacoes do codigo do heroi

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("ola mundo")
+
+let nomeHeroi = "Renatinho"
+let experiencia = 2500
+
+if(experiencia <= 1000){
+    console.log("nivel ferro")
+}else if(experiencia > 1000 && experiencia <= 2000){
+    console.log("nivel bronze")
+}else if(experiencia > 2000 && experiencia <= 5000){
+    console.log("nivel prata")
+}else if(experiencia > 5000 && experiencia <= 7000){
+    console.log("nivel ouro")
+}else if(experiencia > 7000 && experiencia <= 9000){
+    console.log("nivel platina")
+}else if(experiencia > 9000 && experiencia <= 11000){
+    console.log("nivel diamante")
+}else if(experiencia > 11000 && experiencia <= 13000){
+    console.log("nivel ascendente")
+}else if(experiencia > 13000 && experiencia <= 15000){
+    console.log("nivel immortal")
+}
