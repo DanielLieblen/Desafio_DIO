ba78b32d8e6267d5b02a0561a726a20f5d890277
DanielLieblen <plocks.me@gmail.com>
2024-04-29 17:26:13 -0300

    modificacoes no printf

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ let nomeHeroi = "Renatinho"
 let experiencia = 2500
 
 if(experiencia <= 1000){
-    console.log("o heroi de nome" + nomeHeroi + "nivel ferro")
+    console.log("o heroi de nome" + nomeHeroi + "esta no nivel ferro")
 }else if(experiencia > 1000 && experiencia <= 2000){
-    console.log("o heroi de nome" + nomeHeroi + "nivel bronze")
+    console.log("o heroi de nome" + nomeHeroi + "esta no nivel bronze")
 }else if(experiencia > 2000 && experiencia <= 5000){
-    console.log("o heroi de nome" + nomeHeroi + "nivel prata")
+    console.log("o heroi de nome" + nomeHeroi + "esta no nivel prata")
 }else if(experiencia > 5000 && experiencia <= 7000){
-    console.log("o heroi de nome" + nomeHeroi + "nivel ouro")
+    console.log("o heroi de nome" + nomeHeroi + "esta no nivel ouro")
 }else if(experiencia > 7000 && experiencia <= 9000){
-    console.log("o heroi de nome" + nomeHeroi + "nivel platina")
+    console.log("o heroi de nome" + nomeHeroi + "esta no nivel platina")
 }else if(experiencia > 9000 && experiencia <= 11000){
-    console.log("o heroi de nome" + nomeHeroi + "nivel diamante")
+    console.log("o heroi de nome" + nomeHeroi + "esta no nivel diamante")
 }else if(experiencia > 11000 && experiencia <= 13000){
-    console.log("o heroi de nome" + nomeHeroi + "nivel ascendente")
+    console.log("o heroi de nome" + nomeHeroi + "esta no nivel ascendente")
 }else if(experiencia > 13000 && experiencia <= 15000){
-    console.log("o heroi de nome" + nomeHeroi + "nivel immortal")
+    console.log("o heroi de nome" + nomeHeroi + "esta no nivel immortal")
 }
