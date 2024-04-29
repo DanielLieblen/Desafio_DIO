5c88ce6e092305c5585fc9c595696fc8200811d0
DanielLieblen <plocks.me@gmail.com>
2024-04-29 17:24:32 -0300

    modificacoes na impressao

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ let nomeHeroi = "Renatinho"
 let experiencia = 2500
 
 if(experiencia <= 1000){
-    console.log("nivel ferro")
+    console.log("o heroi de nome" + nomeHeroi + "nivel ferro")
 }else if(experiencia > 1000 && experiencia <= 2000){
-    console.log("nivel bronze")
+    console.log("o heroi de nome" + nomeHeroi + "nivel bronze")
 }else if(experiencia > 2000 && experiencia <= 5000){
-    console.log("nivel prata")
+    console.log("o heroi de nome" + nomeHeroi + "nivel prata")
 }else if(experiencia > 5000 && experiencia <= 7000){
-    console.log("nivel ouro")
+    console.log("o heroi de nome" + nomeHeroi + "nivel ouro")
 }else if(experiencia > 7000 && experiencia <= 9000){
-    console.log("nivel platina")
+    console.log("o heroi de nome" + nomeHeroi + "nivel platina")
 }else if(experiencia > 9000 && experiencia <= 11000){
-    console.log("nivel diamante")
+    console.log("o heroi de nome" + nomeHeroi + "nivel diamante")
 }else if(experiencia > 11000 && experiencia <= 13000){
-    console.log("nivel ascendente")
+    console.log("o heroi de nome" + nomeHeroi + "nivel ascendente")
 }else if(experiencia > 13000 && experiencia <= 15000){
-    console.log("nivel immortal")
+    console.log("o heroi de nome" + nomeHeroi + "nivel immortal")
 }
